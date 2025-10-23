1894c4c23be729ddc9c9f23e677d9ac5b6afab85
MissHertz <286863@stud.inn.no>
2025-10-23 12:12:13 +0200

    Includes

---
+++ src/Ball.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Window.h"
+#include "Paddle_1.h"
+#include "Paddle_2.h"
+#include "Paddle_CPU.h"
+#include "Blocks.h"
+
+
+
+
+class Ball
+{
+public:
+};
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,22 @@
 #include <raylib.h>
 #include <iostream>
 
+#include "Window.h"
+#include "Blocks.h"
+#include "Paddle_1.h"
+#include "Paddle_2.h"
+#include "Paddle_CPU.h"
+#include "Ball.h"
+
 using namespace std;
+
 
 
 int main()
 {
+
+
+
 
 	return 0;
 }
