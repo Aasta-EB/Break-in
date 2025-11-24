5ef6f78d4f45c2fb46a96b714e3f655f650b8007
MissHertz <286863@stud.inn.no>
2025-11-24 15:04:19 +0100

    Progress

--- src/FunDrop.h
+++ src/FunDrop.h
@@ -32,44 +32,12 @@ public:
 	Color drawColor = PURPLE;
 	std::vector<Drop> drops;
 
-	FunUpgrade RandomUpgrade()
-	{
-		FunUpgrade upgrade_choices[] = { ENLARGE_BALL, ENLARGE_PADDLE, SPEED_BALL, SPEED_OPPONENT, SPEED_SELF };
-		return upgrade_choices[GetRandomValue(0, 4)] ;
-	}
+	FunUpgrade RandomUpgrade();
 
-	void Spawn(int blockX, int blockY, float direction)
-	{
-		int dropOrNot = GetRandomValue(1,100);
-		if (dropOrNot <= 50)
-		{
-			Drop drop = { {blockX, blockY}, direction * speed_x, true };
-			drops.push_back(drop);
-		}
-	}
+	void Spawn(int blockX, int blockY, float direction);
 
-	void Draw()
-	{
-		for (Drop drop : drops)
-		{
-			if (drop.active)
-			{
-			DrawPoly(drop.position, 6, 25, drop.rotation, drawColor);
-			}
-		}
+	void Draw();
 
-	}
-
-	void Update()
-	{
-		for (Drop& drop : drops)
-		{
-			if (drop.active)
-			{
-				drop.position.x += drop.speed;
-				drop.rotation += 0.7f;
-			}
-		}
-	}
+	void Update();
 };
 
